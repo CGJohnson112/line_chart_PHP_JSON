fae6f8c93038012b163962191dfc9cf1365def2a
Chris Johnson <chrisjohnson112@yahoo.com>
2020-05-22 11:59:13 -0500

    changed chart to show two sets of data

--- js/script.js
+++ js/script.js
@@ -6,34 +6,48 @@ $(function(){
         method: "GET",
         success: function(data) {
             console.log(data);
-            let ontime = [];
-            let score = [];
+            let ontimeId= [];
+            let score2019 = [];
+            let score2020 = [];
 
             for (let i in data) {
-                ontime.push(data[i].ontimeId);
-                score.push(data[i].score);
+                ontimeId.push(data[i].ontimeId);
+                score2019.push(data[i].score2019);
+                score2020.push(data[i].score2020);
             }
 
             var chartdata = {
-                labels: ontime,
+                labels: ontimeId,
                 datasets : [
 
                     {
-                        label: 'Ontime Service by month',
-                        backgroundColor: 'rgba(255, 0, 255, 0.75)',
-                        borderColor: 'rgba(255, 153, 255, 0.75)',
-                        hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-                        hoverBorderColor: 'rgba(200, 200, 200, 1)',
-                        fill: '0',
-                        data: score
+                        label: 'Ontime Service 2019',
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(255, 0, 0, 0.75)',
+                        borderColor: 'rgba(255, 0, 0, 0.75)',
+                        pointHoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+                        pointHoverBorderColor: 'rgba(200, 200, 200, 1)',
+                        data: score2019
 
-                    }
+                    }, {
+                        label: 'Ontime Service 2020',
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(102, 102, 255, 0.75)',
+                        borderColor: 'rgba(102, 102, 255, 0.75)',
+                        pointHoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+                        pointHoverBorderColor: 'rgba(200, 200, 200, 1)',
+                        data: score2020
+
+                    }, 
+
                 ]
             };
 
             let ctx = $("#mycanvas");
 
-            var barGraph = new Chart(ctx, {
+            var LineGraph = new Chart(ctx, {
                 type: 'line',
                 data: chartdata
             });
